303f441b125fa37f78d66a4718bce1d205c1e469
Mick Evans <jaspercayne@gmail.com>
2019-06-08 19:12:28 -0400

    chore: remove crud from prior to dynamic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './navigation/header/header.component';
 import { FooterComponent } from './navigation/footer/footer.component';
+import { AngularComponent } from './pages/angular/angular.component';
+import { FirebaseComponent } from './pages/firebase/firebase.component';
+import { NgmaterialComponent } from './pages/ngmaterial/ngmaterial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AngularComponent,
+    FirebaseComponent,
+    NgmaterialComponent
   ],
   imports: [
     BrowserModule,
