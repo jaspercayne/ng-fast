a6a26d0a01c7ba791b7d2c50d52b5deb1f5536db
Mick Evans <jaspercayne@gmail.com>
2019-06-09 10:07:19 -0400

    fix: add missing import for routermodule

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  imports: [MaterialModule],
+  imports: [RouterModule, MaterialModule],
   declarations : [HeaderComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent]
 })
